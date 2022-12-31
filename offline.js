0fb9e01b3fb4a393b8484d8044f449d99ab6f656
Xneiles <104361401+XnegiL@users.noreply.github.com>
2022-12-31 20:58:48 +0300

    Add files via upload

--- offline.js
+++ offline.js
@@ -1,20 +1,24 @@
 ﻿{
-	"version": 1666984738,
+	"version": 1672509383,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-3.4.1.min.js",
 		"offlineClient.js",
 		"images/sprite-sheet0.png",
+		"images/tiledbackground.png",
 		"images/sprite2-sheet0.png",
-		"images/sprite3-sheet0.png",
-		"images/sprite4-sheet0.png",
-		"images/sprite5-sheet0.png",
+		"images/tiledbackground2.png",
+		"images/particles.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
 		"icon-128.png",
 		"icon-256.png",
-		"loading-logo.png"
+		"loading-logo.png",
+		"c2mp-net.js",
+		"c2mp-peer.js",
+		"c2mp.js",
+		"waker.js"
 	]
 }
